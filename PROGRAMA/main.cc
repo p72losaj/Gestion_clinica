b0d1de81b89e85110214a9c5661c1a9afbdaa8ee
Jaime Lorenzo Sanchez <p72losaj@uco.es>
2019-11-20 18:16:26 +0100

    Indicando funciones del main.cc sobre persona

---
+++ PROGRAMA/main.cc
@@ -0,0 +1,13 @@
+/*
+	Programa principal
+
+		1. Buscar Paciente
+		2. Mostrar Informacion Paciente
+		3. Añadir Paciente
+		4. Modificar Datos Paciente
+		5. Añadir Tratamiento al paciente
+		6. Mostrar tratamiento Paciente
+		7. Eliminar Paciente
+		8. Mostrar Lista Pacientes
+		9. Mostrar Citas Paciente
+*/
