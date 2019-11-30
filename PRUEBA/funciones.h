82b8ab6f93011477e3f3c644501ad816305db598
p72losaj <p72losaj@uco.es>
2019-11-30 11:19:41 +0100

    Añadiendo prueba

---
+++ PRUEBA/funciones.h
@@ -0,0 +1,120 @@
+/*
+	Declaracion de las clases
+*/
+
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+/*
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <list>
+#include <iostream>
+#include <fstream>
+*/
+#include <iostream>
+#include <cstring>
+#include <list>
+#define MAX 100
+using namespace std;
+
+/*
+	Funciones clase paciente
+*/
+
+class Paciente{
+
+	private:
+
+		string nombre;
+		string apellidos;
+		string direccion;
+		string nacimiento;
+		string hospital;
+		string telefono;
+		string historial; 
+		list <Paciente> Pacientes_;
+
+	public:
+
+		/*
+		Constructor de la clase
+		nombre, apellidos, direccion, nacimiento, hospital, telefono, historial
+		*/
+		Paciente(string, string, string, string, string, string, string);
+
+		void addPaciente(Paciente& reg);
+		
+		string GetNombre(){return nombre;}
+		void SetNombre(string cad){ nombre = cad; }
+
+		string GetApellidos(){return apellidos;}
+		void SetApellidos(string cad){ apellidos = cad; }
+
+		string GetDireccion(){return direccion;}
+		void SetDireccion(string cad){ direccion = cad; }
+
+		string GetNacimiento(){return nacimiento;}
+		void SetNacimiento(string cad){ nacimiento = cad; }
+
+		string GetHospital(){return hospital;}
+		void SetHospital(string cad){ hospital = cad; }
+
+		string GetTelefono(){return telefono;}
+		void SetTelefono(string cad){ telefono = cad;}
+
+		string GetHistorial(){return historial;}
+		void SetHistorial(string cad){ historial = cad; }
+
+		bool BuscarPaciente(string cad);
+		
+		inline list <Paciente> GetPacientes(){ return Pacientes_;}
+
+		/*	
+		void AddTratamiento(string cad);
+		void MostrarTratamiento(string cad);
+		void EliminarPaciente(string cad);
+		void MostrarListaPacientes();
+		void MostrarCitasPaciente(string cad);
+	*/
+};
+	
+	/*
+		Funciones clase Cita
+		Clase cita hereda la clase paciente
+	*/
+/*
+	// Obtener la hora de la cita
+	inline string getHora() const { return hora_; }
+	// Modificar la hora de la cita
+	inline string setHora( string cad) { hora_ = cad; }
+	// Obtener la fecha de la cita
+	inline string getFecha() const { return fecha_; }
+	// Modificar la fecha de la cita
+	inline string setFecha( string cad) { fecha_ = cad; }
+	// Obtener el motivo de la cita
+	inline string getMotivo() const { return motivo_; }
+	// Modificar motivo de la cita
+	inline string setMotivo(string cad) { motivo_ = cad;}
+*/
+	/*
+		Funciones clase tratamiento
+
+		Clase tratamiento hereda la clase Persona
+	*/
+/*
+	 // Obtener el nombre del tratamiento
+	inline string getTratamiento() const { return tratamiento_; }
+	// Modificar nombre tratamiento
+	inline string setTratamiento( string cad) { tratamiento_ = cad; }
+	// Obtener fecha comienzo tratamiento
+	inline string getInicio() const { return inicio_; }
+	// Obtener fecha fin tratamiento
+	inline string getFin() const { return fin_; }
+	// Obtener dosis tratamiento
+	inline string getDosis() const { return dosis_; }
+	// Modificar dosis tratamineto
+	inline string setDosis( string cad){ dosis_ cad; }
+	*/
+#endif
---
+++ PRUEBA/main.cc
@@ -0,0 +1,240 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <list>
+#include "funciones.h"
+using namespace std;
+
+
+
+Paciente::Paciente(string n, string a, string d, string fn, string ht, string t, string hm) { 
+   nombre = n;
+   apellidos = a;
+   direccion = d;
+   nacimiento = fn;
+   hospital = ht;
+   telefono = t;
+   historial = hm;
+}
+
+bool Paciente::BuscarPaciente(string cad){
+
+  bool encontrado = false;
+  
+  list <Paciente>:: iterator i;
+  if(Pacientes_.empty()==true) cout<<" Lista vacia"<<endl;
+
+  for(i=Pacientes_.begin(); i!= Pacientes_.end(); i++){
+    if(i->GetNombre() == cad){
+      encontrado = true;
+    }
+  }
+  
+  return encontrado;  
+}
+
+ostream& operator<<(ostream &os, Paciente& reg) {
+    os << "Nombre: " << reg.GetNombre() << endl;
+    os << "Apellidos: "<<reg.GetApellidos()<<endl;
+    os << "Direccion: "<<reg.GetDireccion()<<endl;
+    os << "Fecha_Nacimiento: "<<reg.GetNacimiento()<<endl;
+    os << "Hospital de procedencia: "<<reg.GetHospital()<<endl;
+    os << "Numero de telefono: "<<reg.GetTelefono()<<endl;
+    os << "Historial del paciente: "<<reg.GetHistorial()<<endl;
+   return os;
+}
+
+void Paciente::addPaciente(Paciente& reg)
+{
+  string aux;
+  list <Paciente>::iterator i;
+  fstream file("pacientes.txt", ios::app);
+  if(!file) cout<<"Error acceso fichero"<<endl;
+  else{
+
+    file<<reg.GetNombre()+",";
+    file<<reg.GetApellidos()+",";
+    file<<reg.GetDireccion()+",";
+    file<<reg.GetNacimiento()+",";
+    file<<reg.GetHospital()+",";
+    file<<reg.GetTelefono()<<+",";
+    file<<reg.GetHistorial()<<endl;
+    // Cerramos el fichero
+    file.close(); 
+  }
+
+  // Mostramos la lista por pantalla
+  cout<<" Leyendo lista"<<endl;
+  for(i=Pacientes_.begin(); i!=Pacientes_.end(); i++){
+    cout<<"\tNombre: "<<i->GetNombre()<<endl;
+    cout<<"\tApellidos: "<<i->GetApellidos()<<endl;
+    cout<<"\tDireccion: "<<i->GetDireccion()<<endl;
+    cout<<"\tNacimiento: "<<i->GetNacimiento()<<endl;
+    cout<<"\tHospital: "<<i->GetHospital()<<endl;
+    cout<<"\tTelefono: "<<i->GetTelefono()<<endl;
+    cout<<"\tHistorial: "<<i->GetHistorial()<<endl;
+  }
+}
+
+
+int main(void){
+
+  list <Paciente> Pacientes_;
+  string nombre, apellidos, direccion, nacimiento, hospital, telefono, historial, historial2;
+  Paciente p1( nombre,apellidos,direccion, nacimiento,hospital,telefono,historial);
+  Paciente P(p1);
+  remove("pacientes.txt");
+  int opcion, opcion2;
+  bool encontrado;
+  string cad;
+
+  do{
+
+    cout<<"MENU"<<endl;
+    cout<<"0. Salir del programa"<<endl;
+    cout<<"1. Buscar Paciente"<<endl;
+    cout<<"2. Mostrar informacion de un paciente"<<endl;
+    cout<<"3. Añadir nuevo paciente"<<endl;
+    cout<<"4. Modificar datos paciente"<<endl;
+
+    cout<<"Introduce una opcion: "<<endl;
+    cin>>opcion;
+    
+    switch(opcion){
+
+      case 1:
+
+  		cout<<"Nombre paciente= "<<endl;
+        cin>> nombre;
+        encontrado = P.BuscarPaciente(nombre);
+        if(encontrado == true) cout<<"Paciente encontrado"<<endl;
+        else cout<<"Error. Paciente no encontrado"<<endl;
+        
+
+      break;
+
+      case 2:
+
+        cout<<"Nombre paciente= "<<endl;
+        cin>> nombre;
+        encontrado = P.BuscarPaciente(nombre);
+        if(encontrado == true) cout<<p1<<endl;
+        else cout<<"Error. Paciente no encontrado"<<endl;
+
+      break;
+
+      case 3:
+
+        cout<<"Nombre paciente= "<<endl;
+        cin>> nombre;
+        encontrado = P.BuscarPaciente(nombre);
+        if(encontrado == true) cout<<"Error. Paciente ya existente"<<endl;
+        else
+        {
+        	p1.SetNombre(nombre);
+        	cout<<"Apellidos sin espacios: ";
+        	cin>> apellidos;
+        	p1.SetApellidos(apellidos);
+        	cout<<"Direccion sin espacios: ";
+        	cin>> direccion;
+        	p1.SetDireccion(direccion);
+        	cout<<"Nacimiento paciente: ";
+        	cin >> nacimiento;
+        	p1.SetNacimiento(nacimiento);
+        	cout<<"Hospital privado/publico: ";
+        	cin >> hospital;
+        	p1.SetHospital(hospital);
+        	cout<<"Numero telefono= ";
+        	cin >> telefono;
+        	p1.SetTelefono(telefono);
+        	cout<<"Historial = ";
+        	cin >> historial;
+        	p1.SetHistorial(historial);
+        	P.addPaciente(p1);
+     	}
+
+      break;
+
+      case 4:
+
+		cout<<"Nombre paciente= "<<endl;
+        cin >> nombre;
+        encontrado = P.BuscarPaciente(nombre);
+        if(encontrado == true) 
+        {
+        	do
+        	{
+        		cout<<"0. Volver al menu" <<endl;
+        		cout<<"1. Modificar nombre"<<endl;
+        		cout<<"2. Modificar apellidos"<<endl;
+        		cout<<"3. Modificar direccion"<<endl;
+        		cout<<"4. Modificar fecha nacimiento"<<endl;
+        		cout<<"5. Modificar hospital procedencia"<<endl;
+        		cout<<"6. Modificar numero de telefono"<<endl;
+            	cout<<"7. Modificar historial del paciente"<<endl;
+            	cout<<"Selecciona opcion: "<<endl;
+            	cin >> opcion2;
+            	
+            	if( opcion2 == 1)
+            	{
+            		cout<< "Nombre (sin espacios): ";
+            		cin >> nombre;
+            		p1.SetNombre(nombre);
+            	}
+            
+           		if( opcion2 == 2 )
+            	{
+            		cout<<"Apellidos (sin espacios): ";
+            		cin >> apellidos;
+            		p1.SetApellidos(apellidos);
+            	}
+            
+            	if( opcion2 == 3 )
+            	{
+            		cout<<" Nueva direccion sin espacios: ";
+            		cin >> direccion;
+            		p1.SetDireccion(direccion);
+            	}
+           
+            	if( opcion2 == 4 )
+	        	{
+    	    	    cout <<" Nueva fecha de nacimiento sin espacios: ";
+            	  	cin >> nacimiento;
+            	  	p1.SetNacimiento(nacimiento);
+            	}
+            
+            	if( opcion2==5 )
+            	{
+            		cout<<" Nuevo hospital de procedencia (privado/publico): ";
+					cin>>hospital;
+            		p1.SetHospital(hospital);
+            	}
+            		
+            	if( opcion2==6 )
+            	{
+            		cout<<" Nuevo numero de telefono: ";
+            		cin>>telefono;
+            		p1.SetTelefono(telefono);
+            	}
+            	
+            	if( opcion2 == 7)
+            	{
+            	  historial2 = p1.GetHistorial();
+            	  cout<<" Nuevo historial del paciente: ";
+            	  cin>>historial;
+            	  p1.SetHistorial( (historial2 + ',' + historial));
+            	}
+
+            	cout<<p1<<endl;
+
+          	} while(opcion2 != 0);
+        }
+        else cout<<"Error. Paciente no encontrado"<<endl;
+
+      break;
+    }
+
+  } while(opcion!=0);
+  
+  return 0;
+}
